1d1afbb158a79ebf46efb87b8d44ad597cde3efc
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-09-19 23:56:10 +0530

    InterfaceProductService

---
+++ dream-shop/src/main/java/com/Shop/dream_shop/service/IProductService.java
@@ -0,0 +1,20 @@
+package com.Shop.dream_shop.service;
+
+import com.Shop.dream_shop.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct();
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    Product updateProduct(ProductUpdateRequest product, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrand(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String category, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+
+}
